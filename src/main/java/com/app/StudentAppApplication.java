dd25824bf7bf1c32a3254b9f3fa0d0bf1d5a3fc8
javatechiefriends <rajgoudbommagoni9@gmail.com>
2019-09-12 17:35:20 +0530

    First Commit

--- src/main/java/com/app/StudentAppApplication.java
+++ src/main/java/com/app/StudentAppApplication.java
@@ -6,10 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StudentAppApplication {
 
-	public static void main(String[] args) {
-
-		System.out.println("conflicts purpose");
-		
+	public static void main(String[] args) {		
 		SpringApplication.run(StudentAppApplication.class, args);
 	}
 }
