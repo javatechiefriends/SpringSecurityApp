3be1ec1b5d3bf21f944dfbfbfb9e094ebbb717d0
javatechiefriends <rajgoudbommagoni9@gmail.com>
2019-09-12 17:08:48 +0530

    some changes

--- src/main/java/com/app/StudentAppApplication.java
+++ src/main/java/com/app/StudentAppApplication.java
@@ -8,8 +8,9 @@ public class StudentAppApplication {
 
 	public static void main(String[] args) {
 
-		System.out.println("conflicts purpose");
-		System.out.println("Hello spring Boot World");
+		System.out.println("conflicts purpose111");
+		System.out.println("2222222conflicts purpose2222222222222");
+		System.out.println("QWERTY qwerty Hello @@@@@ spring Boot World");
 		SpringApplication.run(StudentAppApplication.class, args);
 	}
 }
